a14b0ab9cefcf74fc2fd10fd48c3018860167891
joesdesk <jomar.sastrillo@gmail.com>
2018-05-10 22:55:56 -0700

    Moved main code into gui folder to make main.py file almost empty

---
+++ gui/mainwindow.py
@@ -0,0 +1,15 @@
+import wx
+
+class MainWindow(wx.Frame):
+    '''
+    This window is the one containing the main controls and panels
+    of the program.
+    '''
+
+    def __init__(self, parent, title=""):
+        '''
+        Initializes the main window.
+        '''
+
+        # Since we will use a toolbar, we use the wx.Frame
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=title)
