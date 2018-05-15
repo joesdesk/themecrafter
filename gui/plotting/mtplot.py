c17a321afc7815bad89e85e70fff7dd108d1fb8b
joesdesk <jomar.sastrillo@gmail.com>
2018-05-15 11:35:21 -0700

    Examples of plotting in wx

---
+++ gui/plotting/mtplot.py
@@ -0,0 +1,34 @@
+from numpy import arange, sin, pi
+import matplotlib
+matplotlib.use('WXAgg')
+
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wx import NavigationToolbar2Wx
+from matplotlib.figure import Figure
+
+import wx
+
+class CanvasPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.figure = Figure()
+        self.axes = self.figure.add_subplot(111)
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.canvas, 1, wx.LEFT | wx.TOP | wx.GROW)
+        self.SetSizer(self.sizer)
+        self.Fit()
+
+    def draw(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2 * pi * t)
+        self.axes.plot(t, s)
+
+
+if __name__ == "__main__":
+    app = wx.PySimpleApp()
+    fr = wx.Frame(None, title='test')
+    panel = CanvasPanel(fr)
+    panel.draw()
+    fr.Show()
+    app.MainLoop()
