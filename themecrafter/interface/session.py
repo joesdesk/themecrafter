e7e2e46e56aef8b4688f1001ecf91548696c709c
joesdesk <jomar.sastrillo@gmail.com>
2018-05-22 06:19:56 -0700

    New module to abstract user session with package analysis routines

---
+++ themecrafter/interface/session.py
@@ -0,0 +1,12 @@
+#from ..datasets import bgsurvey
+
+class ThemeCrafterSession:
+
+    def __init__(self):
+        pass
+
+    def load_data(self, dataset):
+        self.data = dataset
+
+    def show_data(self, dataset):
+        print("data shown.")
