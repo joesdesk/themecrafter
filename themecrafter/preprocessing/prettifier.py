4c3e03f3d6f0f50027a91a9a3e98d5b3251c505c
joesdesk <jomar.sastrillo@gmail.com>
2018-05-28 14:22:33 -0700

    Cleaned up code

--- themecrafter/preprocessing/prettifier.py
+++ themecrafter/preprocessing/prettifier.py
@@ -42,9 +42,7 @@ class SentenceElement(HtmlElement):
         
         last_stop = 0
         for t in s.tokens:
-            #space = HtmlElement()
-            #space.tag = 'span'
-            #space.insert_element(' '*(t.sloc - last_stop))
+
             space = ' '*(t.sloc - last_stop)
             self.insert_element(space)
             last_stop = t.sloc + len(t.text)
@@ -69,9 +67,7 @@ class DocumentElement(HtmlElement):
         
         last_stop = 0
         for s in d.sentences:
-            #space = HtmlElement()
-            #space.tag = 'span'
-            #space.insert_element(' '*(s.dloc - last_stop))
+
             space = ' '*(s.dloc - last_stop)
             p.insert_element(space)
             last_stop = s.dloc + len(s.text)
@@ -102,15 +98,10 @@ class HtmlCorpus(HtmlElement):
         body = HtmlElement()
         body.tag = 'body'
     
-        # Create line break element
-        lb = HtmlElement()
-        lb.tag = 'br'
-        
         # 
         corpus = Corpus(docs)
         for doc in corpus.docs:
             body.insert_element(DocumentElement(doc))
-            #body.insert_element(lb)
     
         self.insert_element(body)
         
