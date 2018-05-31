58d64d0167d37b52dc9aa39cc00f71611750c393
joesdesk <jomar.sastrillo@gmail.com>
2018-05-30 22:01:37 -0700

    Changes in module now reflected in testing

--- tests/test_preprocessing/test_document.py
+++ tests/test_preprocessing/test_document.py
@@ -1,25 +1,25 @@
 import pytest
 
-from themecrafter.preprocessing.docelements import TokenElement, \
+from themecrafter.preprocessing.nltkplain import TokenElement, \
     SentenceElement, DocumentElement, CorpusElement
 
 
 def test_token_element():
     txt = 'The'
     token = TokenElement(txt)
-    assert token.as_plain_text()==txt
+    assert token.as_plaintext()==txt
     
 
 def test_sentence_element():
     txt = 'The quick brown fox jumped over the lazy dog.'
     sentence = SentenceElement(txt)
-    assert sentence.as_plain_text()==txt
+    assert sentence.as_plaintext()==txt
     
     
 def test_document_element():
     txt = 'An independent clause has the ability to stand alone as a sentence. It always makes a complete thought. A dependent clause cannot stand alone, even though it has a subject and a verb.'
     document = DocumentElement(txt)
-    assert document.as_plain_text()==txt
+    assert document.as_plaintext()==txt
 
     
 if __name__=='__main__':
