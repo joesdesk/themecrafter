047d9248ce62bc32834655f840c0fa947dfb0b65
joesdesk <jomar.sastrillo@gmail.com>
2018-06-01 07:51:37 -0700

    Added class to replace other

--- themecrafter/nlp/nltkbasic.py
+++ themecrafter/nlp/nltkbasic.py
@@ -1,4 +1,7 @@
-# A python class to organize parsing and documents.
+# Using simple nltk process documents into sentences and tokens.
+
+import xml.etree.ElementTree as ET
+#import TreeBuilder, ElementTree#Element, Elementroot, 
 
 from collections import Counter
 
@@ -6,6 +9,103 @@ import pandas as pd
 
 from nltk import pos_tag
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import TreebankWordTokenizer
+
+class NLTKPlain:
+
+    def __init__(self):
+        self.tbuilder = ET.TreeBuilder()
+        
+        self.word_tokenizer = TreebankWordTokenizer()
+        
+    def make_attr(self, id=None, offset=None):
+        attr = {}
+        if id is not None:
+            attr['id'] = str(id)
+        if offset is not None:
+            attr['offset'] = str(offset)
+        return attr
+    
+    def parse_corpus(self, docs, id=None, offset=None):
+        
+        attr = self.make_attr(id, offset)
+        self.tbuilder.start('corpus', attr)
+        
+        for i, doc in enumerate(docs):
+            self.parse_doc(doc, id=i, offset=0) # docs are indep = offset 0
+        
+        self.tbuilder.end('corpus')
+        return self.tbuilder.close()
+        
+    def parse_doc(self, doc, id=None, offset=None):
+        
+        attr = self.make_attr(id, offset)
+        self.tbuilder.start('doc', attr)
+        
+        # Break the document into sentences.
+        k = 0
+        rel_offset = 0
+        sentences = sent_tokenize(doc)
+        for j, s in enumerate(sentences):
+            
+            # Indicate space before sentence
+            rel_offset = doc.find(s, rel_offset)
+            assert rel_offset >= 0, s + ' not in ' + id + ' past ' + str(rel_offset)
+            self.parse_sent(s, j, offset+rel_offset)
+            rel_offset += len(s)
+        
+        self.tbuilder.end('doc')
+        return self.tbuilder.close()
+    
+    def parse_sent(self, sent, id=None, offset=None):
+        
+        attr = self.make_attr(id, offset)
+        self.tbuilder.start('sent', attr)
+        
+        rel_offset = 0
+        
+        # Bug: can't find single quote substrings
+        #spans = self.word_tokenizer.span_tokenize(sent)
+        words = self.word_tokenizer.tokenize(sent)
+        #assert len(spans)==len(words)
+        
+        # Do parts of speech tagging in addition to tokenization
+        poss = pos_tag(words)
+        assert len(poss)==len(words)
+        
+        for k in range(len(words)):
+            
+            # Indicate space before token
+            word, pos = poss[k]
+            
+            # Stupid replacements by package
+            word, rel_offset = self.find(sent, word, rel_offset)
+            assert rel_offset >= 0, word + ', not in :' + sent
+            
+            self.parse_word(word, pos, k, offset+rel_offset)
+            rel_offset += len(word)
+            k += 1
+        
+        self.tbuilder.end('sent')
+        return self.tbuilder.close()
+    
+    def parse_word(self, word, pos, id=None, offset=None):
+    
+        attr = self.make_attr(id, offset)
+        attr['pos'] = pos
+        self.tbuilder.start('tok', attr)
+        
+        self.tbuilder.data(word)
+        
+        self.tbuilder.end('token')
+        return self.tbuilder.close()
+
+    def find(self, sentence, word, offset=0):
+        '''Fixes substitutions done that prevents finding.'''
+        if word in ['``', "''"]:
+            word = '"'
+        
+        return word, sentence.find(word, offset)
 
 
 class BaseElement:
@@ -14,6 +114,8 @@ class BaseElement:
         self.name = ''
         self.attrs = dict()
         self.contents = []
+    
+    
     
     def get_name(self):
         return self.name
@@ -108,15 +210,7 @@ class SentenceElement(BaseElement):
             
             token.set_attr('i', i)
             
-            # Indicate space before token
-            I_new = text.find(w, I)
-            spacelen = I_new - I
-            self.add(' '*spacelen)
-            #token.set_offset(spacelen)
-            
-            # Record location in document
-            token.set_attr('loc', I)
-            I = I_new + len(w)
+           
             
             # Add token
             self.add(token)
@@ -163,7 +257,7 @@ class CorpusElement(BaseElement):
             self.add(document)
             
 
-class NltkPlain(CorpusElement):
+class NltkPlainX(CorpusElement):
     def __init__(self, docs):
         CorpusElement.__init__(self, docs)
         
@@ -211,5 +305,4 @@ class NltkPlain(CorpusElement):
         f = open(file, 'w+')
         f.write(html_text)
         f.close()
-    
     
