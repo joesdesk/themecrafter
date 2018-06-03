c56a3dcd4234ccd5b29b19170830e6ce9e4a14a9
joesdesk <jomar.sastrillo@gmail.com>
2018-06-02 19:18:55 -0700

    Fast matrix construction

--- themecrafter/matrix/castdoc.py
+++ themecrafter/matrix/castdoc.py
@@ -4,17 +4,23 @@
 
 from math import log
 from collections import Counter
-from scipy.sparse import lil_matrix
+
+from numpy import array
+from scipy.sparse import csr_matrix
 
 
 class TF:
     
-    def __init__(self, method='count'):
+    def __init__(self, method='count', vocab=None):
         self.method = method
+        self.vocab = vocab
     
     def fit(self, words):
         '''Returns a dictionary with words as keys and weighted counts as values.'''
-        wc = self.fit_count(words)
+        if self.vocab is None:
+            self.vocab = list(set(words))
+        
+        counts = self.fit_count(words)
         if self.method=='count':
             pass
         if self.method=='binary':
@@ -28,42 +34,45 @@ class TF:
             for w, c in wc.items():
                 wc[w] = log(1 + c)
         
-        return wc
+        return counts
     
     def fit_count(self, words):
         '''Returns a dictionary with words as keys and counts as values.'''
         wc = Counter(words)
-        return dict(wc)
         
+        counts = [0]*len(self.vocab)
+        for i, v in enumerate(self.vocab):
+            c = wc.get(v, 0)
+            counts[i] = c
+            
+        return counts
         
+
+
 class CountMatrix:
     
     def __init__(self):
         self.vocab = None
         
     def fit(self, docs):
-        n_docs = len(docs)
+        ''''''
+        # see: https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html
         
-        tf = TF()
-        wcounts_by_doc = [tf.fit(doc) for doc in docs]
+        indptr = [0]
+        indices = []
+        data = []
+        vocabulary = {}
         
-        vocab = set()
-        for wcs in wcounts_by_doc:
-            for w, c in wcs.items():
-                vocab.add(w)
-        vocab = list(vocab)
-        self.vocab = vocab
-        n_terms = len(vocab)
-        
-        # Now that we know the size of the vocabulary
-        M = lil_matrix((n_docs, n_terms))
-        for i in range(n_docs):
-            for j in range(n_terms):
-                w = vocab[j]
-                M[i,j] = wcounts_by_doc[i].pop(w, 0)
-        return M
+        for d in docs:
+            for term in d:
+                index = vocabulary.setdefault(term, len(vocabulary))
+                indices.append(index)
+                data.append(1)
+            indptr.append(len(indices))
+            
+        return csr_matrix((data, indices, indptr), dtype=int)
 
-
+        
 class TFIDFMatrix:
 
     def __init__(self):
