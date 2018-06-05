19cfa1a773bde4f08f1e62f893a63f963dd22e5f
joesdesk <jomar.sastrillo@gmail.com>
2018-06-05 09:51:22 -0700

    Changed text to list of documents

--- themecrafter/nlp/corpusparser.py
+++ themecrafter/nlp/corpusparser.py
@@ -1,21 +1,30 @@
 from xml.etree.ElementTree import TreeBuilder
-
+from .utils import show_tree
 
 class CorpusParser:
-    '''Creates a corpus element.'''
+    '''Creates a corpus element whose tokens are documents.'''
     
     def __init__(self):
         pass
         
-    def parse(self, text):
+    def parse(self, list_of_docs):
         builder = TreeBuilder()
-        
         builder.start('corpus')
-        builder.data(text)
-        builder.end('corpus')
         
+        for doc in list_of_docs:
+            builder.start('tok')
+            builder.data(doc)
+            builder.end('tok')
+        
+        builder.end('corpus')
         return builder.close()
         
 
 if __name__=='__main__':
-    pass
+    docs = ['The cat flew out of the box.', \
+        'It is raining today.']
+        
+    parser = CorpusParser()
+    tree = parser.parse(docs)
+    
+    show_tree(tree)
