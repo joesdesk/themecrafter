101c00922ce5b2e74b8320639946c44b49507918
joesdesk <jomar.sastrillo@gmail.com>
2018-06-06 10:22:03 -0700

    Made spaceout function more robust

--- themecrafter/output/html2.py
+++ themecrafter/output/html2.py
@@ -9,27 +9,37 @@ class HTMLTransform:
     def __init__(self, xmlstring):
         '''Prepares the soup for formatting by adding spaces where needed.'''
         soup = BeautifulSoup(xmlstring, "xml")
-        docs = list(soup.corpus.children)
+        docs = soup.corpus.contents
         for doc in docs:
             self._spaceout(doc)
         self.docs = docs
         
         # Number of documents and number of pages
         self.n_per_page = 10
-        self.pages = self.paginate(10)
+        self.n_pages = self.paginate(10)
         
-    def _spaceout(self, doc):
+    def _spaceout(self, tag):
         '''Add spaces where appropriate'''
         offset = 0
-        for tag in doc.find_all('tok'):
+        for t in tag.find_all(True, recursive=False):
 
-            new_offset = int(tag['offset'])
-            space_len = max(0, new_offset - offset)
-            space = ' '*space_len
+            if not t.has_attr('offset'):
+                return None
+            
+            loc_offset = int( t.get('offset') )
+            if loc_offset is not None:
 
-            tag.insert_before(space)
-            l = len(tag.string) if tag.string is not None else 0
-            offset = new_offset + l
+                #print(t.string)
+
+                space_len = loc_offset - offset
+                space = ' '*space_len
+
+                #print(t.name)
+                t.insert_before(space)
+                self._spaceout(t)
+
+                text_len = int(t.get('len'))
+                offset = loc_offset + text_len
             
     def _rename_tags(self, soup):
         '''Change XML tags to HTML elements.'''
@@ -100,11 +110,10 @@ class HTMLTransform:
                 
 if __name__=='__main__':
     
-    from ..nlp.session import PreprocessingSession
+    from ..nlp.utils import open_tree, tree2string
     
-    session = PreprocessingSession()
-    session.open_tree('try.xml')
-    xmlstring = session.tree_as_string()
+    tree = open_tree('try.xml')
+    xmlstring = tree2string(tree)
     
     html = HTMLTransform(xmlstring)
     
