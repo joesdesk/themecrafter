26d04b5ac678e9aace4811b150b7d6bf80ada902
joesdesk <jomar.sastrillo@gmail.com>
2018-06-09 05:35:57 -0700

    Ignore gensim warning by saving its top words

--- themecrafter/nlp/tokens.py
+++ themecrafter/nlp/tokens.py
@@ -1,5 +1,32 @@
-# Filter out stop words and punctuations
+# SPECIAL TOKENS
 
+
+# Utilities to write and read list of stopwords from files
+
+def writelines(file, text):
+    with open(file, 'w') as f:
+        for t in text:
+            f.write(t)
+            f.write('\n')
+            
+
+def readlines(file):
+    with open(file, 'r') as f:
+        text = [line.rstrip('\n') for line in f]
+        return text
+        
+        
+# The 1000 most used words
+# http://splasho.com/upgoer5/
+# http://splasho.com/upgoer5/phpspellcheck/dictionaries/1000.dicin
+most_frequent_1000 = readlines('themecrafter/nlp/tokens/top1000en.txt')
+most_frequent_1000 = frozenset(most_frequent_1000)
+
+# from gensim.parsing.preprocessing import STOPWORDS as gensim_stopwords
+gensim_stopwords = readlines('themecrafter/nlp/tokens/gensim_stopwords.txt')
+gensim_stopwords = frozenset(gensim_stopwords)
+
+# python's list of punctuations
 from string import punctuation
 punctuation = frozenset(punctuation)
 
@@ -7,8 +34,5 @@ punctuation = frozenset(punctuation)
 from nltk.corpus import stopwords as nltk_stopwords
 nltk_stopwords = frozenset(nltk_stopwords.words('english'))
 
-from gensim.parsing.preprocessing import STOPWORDS as gensim_stopwords
-
 
 all_stopwords = nltk_stopwords | gensim_stopwords
-
