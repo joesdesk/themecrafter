d4187801350c0eab0430a031d916e360a10d3b1c
joesdesk <jomar.sastrillo@gmail.com>
2018-06-09 07:49:12 -0700

    Modules to aid models

---
+++ themecrafter/preprocessing/groupdict.py
@@ -0,0 +1,41 @@
+# Grouping items in a list but keeping track of
+# the items which were grouped together.
+
+from collections import defaultdict, Counter
+
+
+class GroupedDict:
+    '''Grouping items in a list but keeping track of the items
+    which were grouped together.
+    '''
+    
+    def __init__(self):
+        self.container = defaultdict(list)
+    
+    def group(self, values, by):
+        for k, v in zip(by, values):
+            self.container[k].append(v)
+        return None
+            
+    def items(self):
+        return self.container.items()
+    
+    def values(self):
+        return self.container.values()
+    
+    def keys(self):
+        return self.container.keys()
+    
+    def counts(self):
+        counts = Counter()
+        for k, v in self.items():
+            counts[k] = len(v)
+        return counts.most_common()
+    
+    def pop(self, key, default=None):
+        return self.container.pop(key, default)
+        
+    def get(self, key, default=None):
+        return self.container.get(key, default)
+        
+        
---
+++ themecrafter/preprocessing/labeltransform.py
@@ -0,0 +1,53 @@
+# Adds labels based on a transformation of other labels on the tree.
+
+# https://stackoverflow.com/questions/33587667/extracting-all-nouns-from-a-text-file-using-nltk
+
+from ..nlp.tokens import all_stopwords, punctuation
+from ..nlp.nltklemmatizer import wordnet_lemmatize
+from nltk.util import flatten
+
+
+class LabelTransform:
+    
+    def __init__(self, labelname='new_label_name'):
+        '''A list of tokens, which are tuples indicating the label and pos.'''
+        self.labelname = labelname
+        
+        self.lemmatize = True
+        self.rm_stopwords = True
+        self.rm_punctuation = True
+        self.rm_char_len = 1
+        
+        self.pos_whitelist = []
+        self.label_blacklist = []
+        
+    def fit(self, tree):
+        '''Applies the token transformation throughout the entire tree.'''
+        for t in tree.findall('.//tok[@label]'):
+            label = t.get('label')
+            pos = t.get('pos', None)
+            
+            # Create new label
+            newlabel = label.lower()
+            if self.lemmatize:
+                newlabel = wordnet_lemmatize(newlabel, pos)
+            
+            # Filter new label
+            if len(label ) <self.rm_char_len:
+                newlabel=None
+            if (self.rm_stopwords) and (label in all_stopwords):
+                newlabel=None
+            if (self.rm_punctuation) and (label in punctuation):
+                newlabel=None
+            if label in self.label_blacklist:
+                newlabel=None
+            
+            if (newlabel is not None) and (pos is not None): 
+                if pos not in self.pos_whitelist:
+                    newlabel=None
+            
+            # Append newlabel if survived filtering
+            if newlabel is not None:
+                t.set(self.labelname, newlabel)
+            
+            
