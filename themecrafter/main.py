dcf9cfa60df426253e95668c58100f9fa309f31c
joesdesk <jomar.sastrillo@gmail.com>
2018-06-11 19:01:23 -0700

    Keep widget-widget interactions within mainwindow

--- themecrafter/main.py
+++ themecrafter/main.py
@@ -23,7 +23,7 @@ class Application:
             # See https://wiki.wxpython.org/self.Bind%20vs.%20self.button.Bind
         #self.Bind(wx.EVT_MENU, self.data_loaded, main_menubar)
         self.window.Bind(EVT_DATA_LOAD, self.data_loaded)
-        self.window.topic_list_ctrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.sel_topic)
+        
         
     def start(self):
         self.window.Show()
@@ -55,21 +55,4 @@ class Application:
             self.html.add_cache(topic, sel_ids)
         
         
-    def sel_topic(self, event):
-        '''Handles the selection of a topic.'''
-        
-        # Obtain the topic index
-        index = event.GetIndex()
-        
-        # Get the topic name
-        topic = self.top_nouns.topics[index]
-        
-        # Get html pages from cache
-        if self.html is not None:
-            pages = self.html.cached_pages[topic]
-            self.commentview.set_data(pages)
-            
-        # Obtain the concurrence of the topic
-        concr = self.top_nouns.topic_concurrence[index]
-        
-        self.plot.bar(self.top_nouns.topics, concr)
+    
