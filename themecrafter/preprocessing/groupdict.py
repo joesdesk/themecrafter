6a47dc5234e005856614dc2ef286acb17aaec3ea
joesdesk <jomar.sastrillo@gmail.com>
2018-06-12 17:48:29 -0700

    Added comment

--- themecrafter/preprocessing/groupdict.py
+++ themecrafter/preprocessing/groupdict.py
@@ -13,6 +13,10 @@ class GroupedDict:
         self.container = defaultdict(list)
     
     def group(self, values, by):
+        '''Computes a dictionary whose keys are the unique values in `by`
+        and the corresponding dictionary value is the list of items
+        in `values` corresponding to the value in `by`.
+        '''
         for k, v in zip(by, values):
             self.container[k].append(v)
         return None
