c56520e9da4b6fe321cbd5ca2d883d9738c17e4a
joesdesk <jomar.sastrillo@gmail.com>
2018-06-14 14:55:19 -0700

    Fixed bug

--- themecrafter/preprocessing/__init__.py
+++ themecrafter/preprocessing/__init__.py
@@ -1,4 +1,4 @@
 from .words import AllWords, BagOfWords
 from .counting import CountMatrix
-from .counting count2bin, count2tf, count2lognorm, count2df, count2tfidf
+from .counting import count2bin, count2tf, count2lognorm, count2df, count2tfidf
 
