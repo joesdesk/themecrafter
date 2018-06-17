a105b9dbea7c741e05ecf4720d0807a1b02266bd
joesdesk <jomar.sastrillo@gmail.com>
2018-06-17 09:06:14 -0700

    Create mainframe

--- themecrafter/gui/app.py
+++ themecrafter/gui/app.py
@@ -2,14 +2,22 @@ import wx
 
 
 class Application:
+    '''Container for the application'''
 
     def __init__(self):
         pass
         
     def start(self):
         app = wx.App()
-        frame = wx.Frame(None)
+        frame = MainFrame()
         frame.Show()
         app.MainLoop()
         
         
+class MainFrame(wx.Frame):
+    '''Uses the mainwindow as a base for the main application window.'''
+    
+    def __init__(self):
+        wx.Frame.__init__(self, parent=None, title='ThemeCrafter v0.0')
+        
+    
