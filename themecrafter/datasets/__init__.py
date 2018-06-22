368c90cc2b90f10044c82d06810a31a040e4873a
joesdesk <jomar.sastrillo@gmail.com>
2018-06-22 11:58:26 -0700

    Moved data out of program

--- themecrafter/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-from .basedataset import BaseDataSet
-
-from .newsgroups import NewsGroupsDataSet
-from .bgsurvey import BGSurveyDataSet
-from .gradreports import GradReportsDataSet
-from .studentsreview import StudentsReviewDataSet
-from .sixdaywar import SixDayWarDataSet
